02cde39e031e50181c0e8ba7c9fc08e29456a653
Phigoes <phi_go@outlook.com>
2021-07-10 18:32:44 -0300

    Used type modules

---
+++ js/script.js
@@ -0,0 +1,21 @@
+import initAccordion from './modules/accordion.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import initScrollSuave from './modules/scroll-suave.js';
+import initTabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js';
+import initAnimaNumeros from './modules/anima-numeros.js';
+import initFuncionamento from './modules/funcionamento.js';
+
+initAccordion();
+initModal();
+initAnimacaoScroll();
+initScrollSuave();
+initTabNav();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
+initAnimaNumeros();
+initFuncionamento();
