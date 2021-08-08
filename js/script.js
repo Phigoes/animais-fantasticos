da4aa966e6f940cb9e979ddf46e0d0056c54df84
Phigoes <phi_go@outlook.com>
2021-08-08 18:20:07 -0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+import initAccordion from './modules/accordion.js';
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import initScrollSuave from './modules/scroll-suave.js';
+import initTabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
+import initMenuMobile from './modules/menu-mobile.js';
+import initFuncionamento from './modules/funcionamento.js';
+import initFetchAnimais from './modules/fetch-animais.js';
+import initFetchBitcoin from './modules/fetch-bitcoin.js';
+
+initAccordion();
+initModal();
+initAnimacaoScroll();
+initScrollSuave();
+initTabNav();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
+initFuncionamento();
+initFetchAnimais();
+initFetchBitcoin();
